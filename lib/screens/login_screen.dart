5408c3c733b0f6712d51c6927816407d14d41574
David Gamboa <davidmoisesgamboacel4@gmail.com>
2025-09-04 10:27:30 -0600

    feat(login): add email and password inputs

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -9,19 +9,68 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  // Estado para mostrar/ocultar contraseña
+  bool _obscurePassword = true;
+
   @override
   Widget build(BuildContext context) {
+    //obtener el tamaño de la pantalla
+    final Size size = MediaQuery.of(context).size;
     return Scaffold(
-        // Evita nudge o cámaras frontales para móviles
-        body: SafeArea(
-            child: Column(
-      children: [
-        Expanded(
-            child: RiveAnimation.asset(
-          'assets/animated_login_character.riv',
-          fit: BoxFit.contain,
-        ))
-      ],
-    )));
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 50),
+          child: Column(
+            children: [
+              SizedBox(
+                width: size.width,
+                height: 200,
+                child: RiveAnimation.asset(
+                  'assets/animated_login_character.riv',
+                  fit: BoxFit.contain,
+                ),
+              ),
+              const SizedBox(height: 10),
+              // Campo de texto Email
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  hintText: "Introduce tu Email",
+                  prefixIcon: const Icon(Icons.mail),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              // Campo de texto Contraseña con ojito invertido
+              TextField(
+                obscureText: _obscurePassword,
+                decoration: InputDecoration(
+                  hintText: "Contraseña",
+                  prefixIcon: const Icon(Icons.lock),
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _obscurePassword
+                          ? Icons.visibility // 👁️ normal cuando está oculta
+                          : Icons
+                              .visibility_off, // 🚫 tachado cuando está visible
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _obscurePassword = !_obscurePassword;
+                      });
+                    },
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
